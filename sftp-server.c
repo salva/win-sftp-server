95fd99920937ea552d599bda4a9e600e2bc3904d
Salvador Fandiño <sfandino@yahoo.com>
2016-11-03 15:48:14 +0100

    remove user_from_uid and user_from_gid which do not apply to Windows

--- sftp-server.c
+++ sftp-server.c
@@ -1485,9 +1485,6 @@ w_utimes(wchar_t *name, uint32_t mtime, uint32_t atime)
 	return rc;
 }
 
-typedef unsigned int uid_t;
-typedef unsigned int gid_t;
-
 static void
 attrib_clear(Attrib *a)
 {
@@ -2459,84 +2456,6 @@ process_opendir(uint32_t id)
 
 	xfree(path);
         xfree(pattern);
-}
-
-/* #define	NCACHE	64			/\* power of 2 *\/ */
-/* #define	MASK	(NCACHE - 1)		/\* bits to store with *\/ */
-/* static char * */
-/* user_from_uid(uid_t uid, int nouser) */
-/* { */
-/* 	static struct ncache { */
-/* 		uid_t	uid; */
-/* 		char	*name; */
-/* 	} c_uid[NCACHE]; */
-/* 	static int pwopen; */
-/* 	static char nbuf[15];		/\* 32 bits == 10 digits *\/ */
-/* 	struct passwd *pw; */
-/* 	struct ncache *cp; */
-
-/* 	cp = c_uid + (uid & MASK); */
-/* 	if (cp->uid != uid || cp->name == NULL) { */
-/* 		if (pwopen == 0) { */
-/* #ifdef HAVE_SETPASSENT */
-/* 			setpassent(1); */
-/* #endif */
-/* 			pwopen = 1; */
-/* 		} */
-/* 		if ((pw = getpwuid(uid)) == NULL) { */
-/* 			if (nouser) */
-/* 				return (NULL); */
-/* 			(void)snprintf(nbuf, sizeof(nbuf), "%u", uid); */
-/* 		} */
-/* 		cp->uid = uid; */
-/* 		if (cp->name != NULL) */
-/* 			free(cp->name); */
-/* 		cp->name = strdup(pw ? pw->pw_name : nbuf); */
-/* 	} */
-/* 	return (cp->name); */
-/* } */
-
-/* static char * */
-/* group_from_gid(gid_t gid, int nogroup) */
-/* { */
-/* 	static struct ncache { */
-/* 		gid_t	gid; */
-/* 		char	*name; */
-/* 	} c_gid[NCACHE]; */
-/* 	static int gropen; */
-/* 	static char nbuf[15];		/\* 32 bits == 10 digits *\/ */
-/* 	struct group *gr; */
-/* 	struct ncache *cp; */
-
-/* 	cp = c_gid + (gid & MASK); */
-/* 	if (cp->gid != gid || cp->name == NULL) { */
-/* 		if (gropen == 0) { */
-/* #ifdef HAVE_SETGROUPENT */
-/* 			setgroupent(1); */
-/* #endif */
-/* 			gropen = 1; */
-/* 		} */
-/* 		if ((gr = getgrgid(gid)) == NULL) { */
-/* 			if (nogroup) */
-/* 				return (NULL); */
-/* 			(void)snprintf(nbuf, sizeof(nbuf), "%u", gid); */
-/* 		} */
-/* 		cp->gid = gid; */
-/* 		if (cp->name != NULL) */
-/* 			free(cp->name); */
-/* 		cp->name = strdup(gr ? gr->gr_name : nbuf); */
-/* 	} */
-/* 	return (cp->name); */
-/* } */
-
- char *
-user_from_uid(uid_t uid, int nouser) {
-	return "paco";
-}
-
- char *
-group_from_gid(gid_t gid, int nogroup) {
-	return "carambolas";
 }
 
 static wchar_t *
