305ac0916ba8c7eaa51c73cb2fa6a773dcf51771
Salvador Fandiño <sfandino@yahoo.com>
2016-11-23 12:59:51 +0100

    improve testing diagnostics

--- test.pl
+++ test.pl
@@ -13,6 +13,16 @@ my $temp = "c:/temp";
 my $wine;
 my $delay = 0;
 
+sub sok {
+    my $s = shift;
+    my $builder = Test::More->builder;
+    $builder->ok(@_);
+    unless ($_[0]) {
+        diag "error: " . $s->error;
+        diag "status: " . $s->status;
+    }
+}
+
 GetOptions("save-err|s" => \$save_err,
            "working-dir|w=s" => \$temp,
            "wine|i" => \$wine,
@@ -52,16 +62,16 @@ my $s = Net::SFTP::Foreign->new(open2_cmd => \@cmd, stderr_fh => $errfh);
 diag "child pid: $s->{pid}";
 sleep $delay;
 
-ok($s->test_d("c:/temp"), "temp directory exists");
-ok($s->test_d("c:\\temp"), "forward and back slashes are equivalent");
+sok($s, $s->test_d("c:/temp"), "temp directory exists");
+sok($s, $s->test_d("c:\\temp"), "forward and back slashes are equivalent");
 ok($s->test_d("C:\\tEmP"), "case does not matter either");
 
 $s->mkdir($remote_dir);
 ok -d $local_dir, "local dir is $local_dir";
 is (path_to_wine($s->realpath($remote_dir)), $local_dir, "realpath");
 
-ok $s->test_d($remote_dir), "remote working dir exists";
-ok $s->setcwd($remote_dir), "setcwd";
+sok $s, $s->test_d($remote_dir), "remote working dir exists";
+sok $s, $s->setcwd($remote_dir), "setcwd";
 is (path_to_wine($s->realpath(".")), $local_dir, "realpath");
 
 my $rfn = "data.txt";
@@ -80,7 +90,7 @@ my $data = <<EOD;
 EOD
 
 is((print {$rfh} $data), length $data, "print");
-ok $s->fsync($rfh), "fsync";
+sok $s, $s->fsync($rfh), "fsync";
 
 ok(opendir my $ldh, $local_dir);
 my @le = sort readdir $ldh;
@@ -92,31 +102,31 @@ my @re = sort map { $_->{filename} } $s->readdir($rdh);
 
 is "@re", "@le";
 
-ok ($s->test_e($rfh));
-ok (!$s->test_d($rfh));
+sok ($s, $s->test_e($rfh));
+sok ($s, !$s->test_d($rfh));
 my $a = $s->stat($rfn);
 is $a->perm & 0777, 0600, sprintf("permissions 0%o", $a->perm);
 
 $s->remove("r-$rfn");
-ok (!$s->test_e("r-$rfn"), "new file is not there yet");
-ok($s->rename($rfn, "r-$rfn"), "rename");
-ok (!$s->test_e($rfn), "old file is gone");
-ok ($s->test_e("r-$rfn"), "new file is there");
-ok ($s->hardlink($rfn, "r-$rfn"), "hardlink");
-ok ($s->test_e($rfn), "old file is there again");
-ok ($s->remove("r-$rfn"), "unlink");
+sok($s, !$s->test_e("r-$rfn"), "new file is not there yet");
+sok($s, $s->rename($rfn, "r-$rfn"), "rename");
+sok($s, !$s->test_e($rfn), "old file is gone");
+sok($s, $s->test_e("r-$rfn"), "new file is there");
+sok($s, $s->hardlink($rfn, "r-$rfn"), "hardlink");
+sok($s, $s->test_e($rfn), "old file is there again");
+sok($s, $s->remove("r-$rfn"), "unlink");
 SKIP: {
     skip "DeleteFileW is lazy in Wine", 1 if $wine;
-    ok (!$s->test_e("r-$rfn"), "new file is finally gone");
+    sok($s, !$s->test_e("r-$rfn"), "new file is finally gone");
 };
 
 my $data1 = do { undef $/; <$rfh2> };
 is ($data1, $data, "read");
 
 is (tell $rfh2, length $data, "tell");
-ok seek($rfh2, 0, 0), "seek";
+sok $s, seek($rfh2, 0, 0), "seek";
 is((print {$rfh1} $data), length $data, "append");
-ok ($s->flush($rfh1), "fsync append");
+sok($s, $s->flush($rfh1), "fsync append");
 
 my $data2 = do { undef $/; <$rfh2> };
 is ($data2, "$data$data", "read appended");
@@ -124,18 +134,18 @@ is ($data2, "$data$data", "read appended");
 my $a1 = $s->stat($rfh2);
 ok($a1, "fstat");
 is ($a1->size, 2 * length $data);
-ok($s->truncate($rfn, length $data), "truncate");
+sok($s, $s->truncate($rfn, length $data), "truncate");
 my $a2 = $s->stat($rfn);
 is ($a2->size, length $data, "stat");
-ok($s->truncate($rfh, 0), "ftruncate");
+sok($s, $s->truncate($rfh, 0), "ftruncate");
 my $a3 = $s->stat($rfh1);
 is ($a3->size, 0, "fstat again");
 is ((print {$rfh1} $data), length $data, "append after truncate");
-ok ($s->fsync($rfh1), "fsync again");
+sok($s, $s->fsync($rfh1), "fsync again");
 my $a4 = $s->stat($rfn);
 is ($a4->size, length $data);
-ok (!defined <$rfh2>, "reading at EOF");
-ok $s->seek($rfh2, 0, 0), "seek to beginning";
+sok($s, !defined <$rfh2>, "reading at EOF");
+sok $s, $s->seek($rfh2, 0, 0), "seek to beginning";
 my $data3 = do { undef $/; <$rfh2> };
 is ($data3, $data, "reread from the beginning");
 
@@ -143,23 +153,23 @@ SKIP: {
     skip "symlinks are not supported by Wine" if $wine;
 
 $s->remove("sl1-$rfn");
-ok(!$s->test_e("sl1-$rfn"), "symbolic link does not exist");
-ok($s->symlink("sl1-$rfn", $s->realpath($rfn)), "abs symlink");
+sok($s, !$s->test_e("sl1-$rfn"), "symbolic link does not exist");
+sok($s, $s->symlink("sl1-$rfn", $s->realpath($rfn)), "abs symlink");
 #ok($s->symlink("sl1-$rfn", $rfn));
 is($s->readlink("sl1-$rfn"), $s->realpath($rfn));
 
 $s->remove("sl2-$rfn");
-ok(!$s->test_e("sl2-$rfn"), "symbolic link does not exist");
-ok($s->symlink("sl2-$rfn", $rfn), "relative symlink");
+sok($s, !$s->test_e("sl2-$rfn"), "symbolic link does not exist");
+sok($s, $s->symlink("sl2-$rfn", $rfn), "relative symlink");
 is($s->readlink("sl2-$rfn"), $rfn, "read relative symlink");
 
 my $a5 = $s->lstat("sl2-$rfn");
-ok($a5, "lstat");
+sok($s, $a5, "lstat");
 is($a5 && $a5->perm, 0120777, "symlink perms");
 is($a5 && $a5->size, 0, "symlink size");
 
 my $a6 = $s->stat("sl2-$rfn");
-ok($a6, "stat");
+sok($s, $a6, "stat");
 my $perm6 = ($a6 && $a6->perm) // 0;
 isnt($perm6, 0120777, "resolved symlink perms");
 is($perm6 & 0170000, 0100000, "resolved symlink is a regular file");
@@ -167,10 +177,10 @@ is($perm6 & 0700, 0600, "resolved symlink has read and write permissions");
 is($a6 && $a6->size, length($data), "resolved symlink size");
 
 my $fh7 = $s->open("sl2-$rfn", SSH2_FXF_READ);
-ok($fh7, "open following link");
+sok($s, $fh7, "open following link");
 
 my $a7 = $s->stat($fh7); # this is a fstat under the hood
-ok($a7);
+sok($s, $a7);
 my $perm7 = ($a7 && $a7->perm) // 0;
 is ($perm7 & 0170000, 0100000, "fstat regular file");
 is ($perm7 & 0700, 0600, "fstat returns read and write permissions");
@@ -179,21 +189,21 @@ my $old_time = time() - 100;
 
 is($s->utime($fh7, $old_time, $old_time), undef, "utime with fsetstat and not enough permissions fails");
 $old_time += 10;
-ok($s->utime($rfh1, $old_time, $old_time), "utime with fsetstat and write permissions succeeds");
+sok($s, $s->utime($rfh1, $old_time, $old_time), "utime with fsetstat and write permissions succeeds");
 $a7 = $s->stat($fh7);
-ok($a7, "fstat again");
+sok($s, $a7, "fstat again");
 my $mtime7 = ($a7 && $a7->mtime) // 0;
 is($mtime7, $old_time, "fstat mtime");
 
 $old_time += 10;
-ok($s->utime($rfh, $old_time, $old_time), "utime with setstat");
+sok($s, $s->utime($rfh, $old_time, $old_time), "utime with setstat");
 my $a8 = $s->stat($rfh);
-ok($a8, "stat 8");
+sok($s, $a8, "stat 8");
 my $mtime8 = ($a8 && $a8->mtime) // 0;
 is ($mtime8, $old_time);
 
 my $a9 = $s->stat("sl1-$rfn");
-ok($a9, "stat 9");
+sok($s, $a9, "stat 9");
 my $mtime9 = ($a9 && $a9->mtime) // 0;
 is($mtime9, $old_time, "mtime following symlink");
 
@@ -204,17 +214,17 @@ my $fh10 = $s->open($long_file_name, SSH2_FXF_WRITE | SSH2_FXF_CREAT);
 
 TODO: {
     local $TODO = "support for long file names is still missing";
-    ok($fh10, "open file with very long file name");
+    sok($s, $fh10, "open file with very long file name");
 };
 
 };
 
-ok($s->disconnect, "disconnect");
+sok($s, $s->disconnect, "disconnect");
 
 my $s2 = Net::SFTP::Foreign->new(open2_cmd => [@cmd, '/d', $temp],
                                  stderr_fh => $errfh);
 ok($s2, "launch second process");
-ok($s2->setcwd($remote_dir), "check /d");
-ok($s2->test_e($rfn), "check /d");
+sok($s2, $s2->setcwd($remote_dir), "check /d");
+sok($s2, $s2->test_e($rfn), "check /d");
 
 done_testing();
